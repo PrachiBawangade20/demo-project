93772d0149a428050a1ec0aa881843e170d10d4b
prachibawangade-208 <prachi.dharampal@boomingbulls.com>
2026-05-13 16:01:23 +0530

    udpated changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div style={{ textAlign: "center", marginTop: "100px" }}>
-      <h1>Demo Project 🚀</h1>
+      <h1>Demo Project - v1🚀</h1>
       <p>Successfully deployed using Vercel</p>
     </div>
   );
