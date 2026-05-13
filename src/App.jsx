b6b72a29dc2e9621569980b0730c8b25ee2ca78b
prachibawangade-208 <prachi.dharampal@boomingbulls.com>
2026-05-13 15:46:19 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "100px" }}>
+      <h1>Demo Project 🚀</h1>
+      <p>Successfully deployed using Vercel</p>
+    </div>
+  );
+}
+
+export default App;
